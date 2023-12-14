db4b4a353a5bddb98d92774b82eac4c12e55f509
Dhanvi Chagalamarri <146175805+Dhanvi9393@users.noreply.github.com>
2023-12-14 11:26:40 +0530

    QuestionAnswer.java

--- QuestionAnswer.java
+++ QuestionAnswer.java
@@ -5,24 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 public class QuestionAnswer {
 
     public static String question[] ={
-            "How many states are there in India?",
-            "Which is the National Animal of India?",
-            "What is the capital of India?",
-            "Who was the First President of India?",
-            "What is the largest river among these?"
+            "How many Rivers Flow in India?",
+            "Which is the National vegetable of India?",
+            "Which is the largest ocean among this?",
+            "Where can we see the milky way in India?",
+            "What is the world`s most popular song on youtube?"
     };
      public static String choices[][] = {
-             {"36","28","13","12"},
-             {"Cow","Elephant","Tiger","Lion"},
-             {"Mumbai","New Delhi","Hyderabad","Banglore"},
-             {"C. Rajagopalachari","Dr. Rajendra Prasad","P Jawaharlal Nehru","Lord MountBatten"},
-             {"Ganga","Yamuna","Krishna","Narmada"}
+             {"500+","600+","400+","700+"},
+             {"Brinjal","Pumpkin","Green chillies","Red chillies"},
+             {"Pacific Ocean","Atlantic Ocean","Indian Ocean","Southern Ocean"},
+             {"Kerala","Himachal pradesh","Delhi","Goa"},
+             {"Baby shark Dance","Happy Birthday","It`s a small world","Despacito"}
      };
 
      public static String correctAnswers[] = {
-             "28",
-             "Tiger",
-             "New Delhi",
-             "Dr. Rajendra Prasad",
-             "Ganga"};
+             "400+",
+             "Pumpkin",
+             "Pacific Ocean",
+             "Himachal Pradesh",
+             "Baby shark Dance"};
 }
